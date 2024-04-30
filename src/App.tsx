39579c675b93890d4f06c5bd7cce614f37545719
Anjan Talatam <tspa2117@gmail.com>
2024-04-30 22:43:47 +0530

    feat: setup primary theme, dark-light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
     <>
-      <Button>Click here</Button>
+      <ThemeProvider>
+        <Button className="bg-primary">Click here</Button>
+        <ModeToggle />
+      </ThemeProvider>
     </>
   );
 }
